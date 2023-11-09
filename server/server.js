24cb2d96a4ecfcec47ae4f07f7c30ced443d9c87
Shantnu Garg <gargshantnu@gmail.com>
2023-11-09 21:46:08 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,93 +1,153 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const multer = require('multer');
-const jwt = require('jsonwebtoken');
-const { spawn } = require('child_process');
-const path = require('path');
+const express = require("express");
+const bodyParser = require("body-parser");
+const multer = require("multer");
+const { spawn } = require("child_process");
+const path = require("path");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
-app.use(express.static('uploads'));
+app.use(express.static("uploads"));
 
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
-const authenticate = (req, res, next) => {
-  const token = req.header('Authorization');
-  if (!token || token !== 'dummyToken') return res.status(401).send('Access denied.');
-  next();
-};
+// const authenticate = (req, res, next) => {
+//   const token = req.header('Authorization');
+//   if (!token || token !== 'dummyToken') return res.status(401).send('Access denied.');
+//   next();
+// };
 
 // Route for file upload
-app.post('/upload', upload.single('file'), (req, res) => {
-  // Save the uploaded file to the uploads folder
-  const fileName = `${Date.now()}_${req.file.originalname}`;
-  const filePath = path.join(__dirname, 'uploads', fileName);
+app.post("/upload", upload.single("file"), (req, res) => {
+    // Save the uploaded file to the uploads folder
+    const fileName = `${Date.now()}_${req.file.originalname}`;
+    const filePath = path.join(__dirname, "uploads", fileName);
 
-  require('fs').writeFileSync(filePath, req.file.buffer);
+    require("fs").writeFileSync(filePath, req.file.buffer);
 
-  res.status(200).json({ fileName });
+    res.status(200).json({ fileName });
 });
 
 // Route for editing properties
-app.post('/edit', authenticate, (req, res) => {
-  const { fileName, scale, position } = req.body;
+app.post(
+    "/edit",
+    // authenticate,
+    (req, res) => {
+        const { fileName, scale, position } = req.body;
 
-  // Call the Blender script to edit properties
-  const editProcess = spawn('blender', [
-    '--background',
-    '--python', path.join(__dirname, 'blender-scripts', 'edit_properties.py'),
-    '--', fileName, scale, position
-  ]);
+        // Call the Blender script to edit properties
+        const editProcess = spawn("blender", [
+            "--background",
+            "--python",
+            path.join(__dirname, "blender-scripts", "edit_properties.py"),
+            "--",
+            fileName,
+            scale,
+            position,
+        ]);
 
-  editProcess.stdout.on('data', (data) => {
-    console.log(`Blender Edit Output: ${data}`);
-  });
+        editProcess.stdout.on("data", (data) => {
+            console.log(`Blender Edit Output: ${data}`);
+        });
 
-  editProcess.stderr.on('data', (data) => {
-    console.error(`Blender Edit Error: ${data}`);
-  });
+        editProcess.stderr.on("data", (data) => {
+            console.error(`Blender Edit Error: ${data}`);
+        });
 
-  editProcess.on('close', (code) => {
-    if (code === 0) {
-      res.status(200).send('Properties edited successfully');
-    } else {
-      res.status(500).send('Failed to edit properties');
+        editProcess.on("close", (code) => {
+            if (code === 0) {
+                res.status(200).send("Properties edited successfully");
+            } else {
+                res.status(500).send("Failed to edit properties");
+            }
+        });
     }
-  });
-});
+);
 
 // Route for exporting as GLB
-app.post('/export', authenticate, (req, res) => {
-  const { fileName } = req.body;
+app.post(
+    "/export/glb",
+    // authenticate,
+    (req, res) => {
+        const { fileName } = req.body;
+        try {
+            // Call the Blender script to export as GLB
+            const exportProcess = spawn("blender", [
+                "--background",
+                "--python",
+                path.join(__dirname, "blender-scripts", "export_glb.py"),
+                "--",
+                fileName,
+            ]);
 
-  // Call the Blender script to export as GLB
-  const exportProcess = spawn('blender', [
-    '--background',
-    '--python', path.join(__dirname, 'blender-scripts', 'export_glb.py'),
-    '--', fileName
-  ]);
+            exportProcess.stdout.on("data", (data) => {
+                console.log(`Blender Export Output: ${data}`);
+            });
 
-  exportProcess.stdout.on('data', (data) => {
-    console.log(`Blender Export Output: ${data}`);
-  });
+            exportProcess.stderr.on("data", (data) => {
+                console.error(`Blender Export Error: ${data}`);
+            });
 
-  exportProcess.stderr.on('data', (data) => {
-    console.error(`Blender Export Error: ${data}`);
-  });
-
-  exportProcess.on('close', (code) => {
-    if (code === 0) {
-      res.status(200).sendFile(path.join(__dirname, 'uploads', `${fileName}.glb`));
-    } else {
-      res.status(500).send('Failed to export as GLB');
+            exportProcess.on("close", (code) => {
+                if (code === 0) {
+                    res.status(200).sendFile(
+                        path.join(__dirname, "uploads", `${fileName}.glb`)
+                    );
+                } else {
+                    res.status(500).send("Failed to export as GLB");
+                }
+            });
+        } catch (e) {
+            console.log("Error", e);
+            res.status(500).send("Failed to export as .fbx");
+        }
     }
-  });
-});
+);
+
+app.post(
+    "/export/fbx",
+    // authenticate,
+    (req, res) => {
+        const { fileName } = req.body;
+
+        try {
+            // Call the Blender script to export as GLB
+            const exportProcess = spawn("blender", [
+                "--background",
+                "--python",
+                path.join(__dirname, "blender-scripts", "export_fbx.py"),
+                "--",
+                fileName,
+            ]);
+
+            exportProcess.stdout.on("data", (data) => {
+                console.log(`Blender Export to .fbx Output: ${data}`);
+            });
+
+            exportProcess.stderr.on("data", (data) => {
+                console.error(`Blender Export to .fbx Error: ${data}`);
+            });
+
+            exportProcess.on("close", (code) => {
+                console.log("Close", code);
+                if (code === 0) {
+                    res.status(200).sendFile(
+                        path.join(__dirname, "uploads", `${fileName}.glb`)
+                    );
+                } else {
+                    res.status(500).send("Failed to export as .fbx");
+                }
+            });
+        } catch (e) {
+            console.log("Error", e);
+            res.status(500).send("Failed to export as .fbx");
+        }
+    }
+);
 
 // Start the server
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on port ${port}`);
 });
