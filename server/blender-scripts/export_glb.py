fc6414842ed67d83da3e33f1a3252f62eca43cb0
Shantnu Garg <gargshantnu@gmail.com>
2023-11-09 18:27:22 +0530

    Update export_glb.py

--- server/blender-scripts/export_glb.py
+++ server/blender-scripts/export_glb.py
@@ -1,17 +1,26 @@
 import bpy
 import sys
 
+# blender --background --python .\export_glb.py -- "samba_dancing"
+
+bpy.ops.object.select_all(action='DESELECT')
+bpy.ops.object.select_by_type(type='MESH')
+bpy.ops.object.delete()
+
 # Get command line arguments
-_, fileName = sys.argv
+print(sys.argv)
+
+fileName = sys.argv[5]
+
+# print(fileName)
 
 # Load the model
-bpy.ops.import_scene.fbx(filepath=f"uploads/{fileName}")
+bpy.ops.import_scene.fbx(filepath=f"../uploads/{fileName}.fbx")
 
-# Access the imported object
-obj = bpy.context.active_object
 
 # Export as GLB
-bpy.ops.export_scene.gltf(filepath=f"uploads/{fileName}.glb", export_format='GLB')
+bpy.ops.export_scene.gltf(filepath=f"../uploads/{fileName}.glb", export_format='GLB')
+
 
 # Quit Blender
 bpy.ops.wm.quit_blender()
