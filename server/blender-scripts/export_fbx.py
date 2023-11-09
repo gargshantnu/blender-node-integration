69a85bc3a412c2a4bc997daad8f20d1fda048d7a
Shantnu Garg <gargshantnu@gmail.com>
2023-11-09 21:46:12 +0530

    Update export_fbx.py

--- server/blender-scripts/export_fbx.py
+++ server/blender-scripts/export_fbx.py
@@ -18,12 +18,14 @@ print(fileName)
 # Load the model
 
 # todo this relative path might not work in ec2
-bpy.ops.import_scene.gltf(filepath=f"../uploads/{fileName}.glb")
+# bpy.ops.import_scene.gltf(filepath=f"../uploads/{fileName}.glb")
+bpy.ops.import_scene.gltf(filepath=f"uploads/{fileName}.glb")
 
 # bpy.ops.object.select_all(action='SELECT')
 
 # Export as GLB
-bpy.ops.export_scene.fbx(filepath=f"../uploads/{fileName}.fbx")
+# bpy.ops.export_scene.fbx(filepath=f"../uploads/{fileName}.fbx")
+bpy.ops.export_scene.fbx(filepath=f"uploads/{fileName}.fbx")
 
 
 # Quit Blender
