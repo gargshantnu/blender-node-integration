bcfed4515e93128f1df1ee8142577cf6d34d884c
Shantnu Garg <gargshantnu@gmail.com>
2023-11-09 18:26:06 +0530

    Create export_fbx.py

---
+++ server/blender-scripts/export_fbx.py
@@ -0,0 +1,28 @@
+import bpy
+import sys
+
+# blender --background --python .\export_fbx.py -- "Megalodon"
+
+bpy.ops.object.select_all(action='DESELECT')
+bpy.ops.object.select_by_type(type='MESH')
+bpy.ops.object.delete()
+
+
+
+# Get command line arguments
+print(sys.argv)
+
+fileName = sys.argv[5]
+
+print(fileName)
+# Load the model
+bpy.ops.import_scene.gltf(filepath=f"../uploads/{fileName}.glb")
+
+# bpy.ops.object.select_all(action='SELECT')
+
+# Export as GLB
+bpy.ops.export_scene.fbx(filepath=f"../uploads/{fileName}.fbx")
+
+
+# Quit Blender
+bpy.ops.wm.quit_blender()
