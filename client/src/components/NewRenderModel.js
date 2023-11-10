1de67957cae7b6fc5cbfcaa3ef90ee6c2ce40407
Shantnu Garg <gargshantnu@gmail.com>
2023-11-10 15:23:50 +0530

    Update NewRenderModel.js

--- client/src/components/NewRenderModel.js
+++ client/src/components/NewRenderModel.js
@@ -1,67 +1,168 @@
-import React, { useEffect, useRef } from 'react';
-// import * as THREE from 'three'
-// import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
-// import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader'
-// import Stats from 'three/examples/jsm/libs/stats.module'
-import Model from 'react-3dmodelx';
+import React, { useEffect, useRef, useState } from "react";
+import * as THREE from "three";
+import Stats from "three/addons/libs/stats.module.js";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import { FBXLoader } from "three/addons/loaders/FBXLoader.js";
 
+const FbxLoader = ({ editProperties }) => {
+    let camera, scene, renderer, stats;
 
-// const RenderModel = ({ editProperties }) => {
-  // const containerRef = useRef();
+    window.shantnu ? window.shantnu.push(1) : (window.shantnu = [1]);
+    console.log("fbx loader", new Date().getTime());
 
-  // useEffect(() => {
-  //   const { file, scale, position } = editProperties;
-  //   if (!file) return;
+    const [isPlaying, setIsPlaying] = useState(true);
 
-    // const scene = new THREE.Scene();
-    // const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    // const renderer = new THREE.WebGLRenderer();
+    function togglePlayPause() {
+        console.log("is playing", isPlaying, !isPlaying);
+        setIsPlaying(!isPlaying);
+    }
+    const clock = new THREE.Clock();
 
-    // renderer.setSize(window.innerWidth, window.innerHeight);
-    // containerRef.current.appendChild(renderer.domElement);
+    let mixer;
 
-    // const loadModel = () => {
-    //   const loader = file.name.endsWith('.fbx') ? new FBXLoader() : new GLTFLoader();
+    function init() {
+        console.log("init animations");
+        const container = document.createElement("div");
+        document.body.appendChild(container);
 
-    //   loader.load(URL.createObjectURL(file), (model) => {
-    //     model.scale.set(scale, scale, scale);
-    //     model.position.set(position, position, position);
+        // const playPauseButton = document.createElement("button");
+        // playPauseButton.textContent = "Play/Pause";
+        // playPauseButton.addEventListener("click", togglePlayPause);
+        // document.body.appendChild(playPauseButton);
 
-    //     scene.add(model);
+        camera = new THREE.PerspectiveCamera(
+            45,
+            window.innerWidth / window.innerHeight,
+            1,
+            2000
+        );
+        camera.position.set(100, 200, 300);
 
-    //     camera.position.z = 5;
+        scene = new THREE.Scene();
+        scene.background = new THREE.Color(0xa0a0a0);
+        scene.fog = new THREE.Fog(0xa0a0a0, 200, 1000);
 
-    //     const animate = () => {
-    //       requestAnimationFrame(animate);
+        const hemiLight = new THREE.HemisphereLight(0xffffff, 0x444444, 5);
+        hemiLight.position.set(0, 200, 0);
+        scene.add(hemiLight);
 
-    //       // Add any animations or updates here
+        const dirLight = new THREE.DirectionalLight(0xffffff, 5);
+        dirLight.position.set(0, 200, 100);
+        dirLight.castShadow = true;
+        dirLight.shadow.camera.top = 180;
+        dirLight.shadow.camera.bottom = -100;
+        dirLight.shadow.camera.left = -120;
+        dirLight.shadow.camera.right = 120;
+        scene.add(dirLight);
+        // ground
+        const mesh = new THREE.Mesh(
+            new THREE.PlaneGeometry(2000, 2000),
+            new THREE.MeshPhongMaterial({ color: 0x999999, depthWrite: false })
+        );
+        mesh.rotation.x = -Math.PI / 2;
+        mesh.receiveShadow = true;
+        scene.add(mesh);
 
-    //       renderer.render(scene, camera);
-    //     };
+        const grid = new THREE.GridHelper(2000, 20, 0x000000, 0x000000);
+        grid.material.opacity = 0.2;
+        grid.material.transparent = true;
+        scene.add(grid);
 
-    //     animate();
-    //   });
-    // };
+        // model
+        const loader = new FBXLoader();
+        // samba_dancing
+        // stanford-bunny
+        // nurbs
+        loader.load("samba_dancing.fbx", function (object) {
+            mixer = new THREE.AnimationMixer(object);
 
-    // loadModel();
+            const action = mixer.clipAction(object.animations[0]);
+            action.play();
 
-    // // Cleanup on unmount
-    // return () => {
-    //   // Dispose renderer
-    //   renderer.dispose();
-    // };
-  // }, [editProperties]);
+            object.traverse(function (child) {
+                if (child.isMesh) {
+                    child.castShadow = true;
+                    child.receiveShadow = true;
+                }
+            });
 
-  // return <div ref={containerRef}></div>;
-//   return  <div style={{ maxWidth: 800, width: '100%', height: 400, margin: 'auto' }}>
-//   <Model.FBX src="./Samba%20Dancing.fbx" />
-// </div>
-// };
+            scene.add(object);
+        });
 
-// export default RenderModel;
+        renderer = new THREE.WebGLRenderer({ antialias: true });
 
-export default () => (
-  <div style={{ maxWidth: 800, width: '100%', height: 400, margin: 'auto' }}>
-    <Model.FBX src="./Samba%20Dancing.fbx" />
-  </div>
-);
+        window.renderer = renderer;
+
+        renderer.setPixelRatio(window.devicePixelRatio);
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        renderer.shadowMap.enabled = true;
+        container.appendChild(renderer.domElement);
+
+        const controls = new OrbitControls(camera, renderer.domElement);
+        controls.target.set(0, 100, 0);
+        controls.update();
+
+        window.addEventListener("resize", onWindowResize);
+
+        // stats
+        stats = new Stats();
+        // container.appendChild(stats.dom);
+    }
+
+    function onWindowResize() {
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+
+        renderer.setSize(window.innerWidth, window.innerHeight);
+    }
+
+    // const animate = useRef(() => {});
+    const animate = () => {
+        console.log("isPlaying in animate ", isPlaying);
+        if (!isPlaying) return;
+        requestAnimationFrame(animate);
+
+        const delta = clock.getDelta();
+
+        if (mixer) mixer.update(delta);
+
+        renderer.render(scene, camera);
+
+        stats.update();
+    };
+    // useEffect(() => {
+    //   animate.current = () => {
+    //     if (!isPlaying) return;
+    //     requestAnimationFrame(animate.current);
+
+    //     const delta = clock.getDelta();
+
+    //     if (mixer) mixer.update(delta);
+
+    //     renderer.render(scene, camera);
+
+    //     stats.update();
+    //   };
+    // }, [isPlaying]);
+
+    const isLoadedRef = useRef(false);
+
+    useEffect(() => {
+        // const { file, scale, position } = editProperties;
+        // if (!file) return;
+        if (!isLoadedRef.current) {
+            init();
+            // animate.current();
+            animate();
+            isLoadedRef.current = true;
+        }
+    }, []);
+    return (
+        <div className="App">
+            Hello
+            <button onClick={togglePlayPause}>Play/Pause</button>
+        </div>
+    );
+};
+
+export default FbxLoader;
