c5e41e60871fc77e71420affba11754173ec5ebf
Shantnu Garg <gargshantnu@gmail.com>
2023-11-10 18:01:26 +0530

    Update FileUpload.js

--- client/src/components/FileUpload.js
+++ client/src/components/FileUpload.js
@@ -1,38 +1,38 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-const FileUpload = ({ onFileUpload }) => {
-  const [file, setFile] = useState(null);
+const FileUpload = ({ onFileChange, setUploadedFile }) => {
+    const [file, setFile] = useState(null);
 
-  const handleFileChange = (e) => {
-    setFile(e.target.files[0]);
-    onFileUpload(e.target.files[0]);
-  };
+    const handleFileChange = (e) => {
+        setFile(e.target.files[0]);
+        onFileChange(e.target.files[0]);
+    };
 
-  const handleUpload = () => {
-    if (file) {
-      const formData = new FormData();
-      formData.append('file', file);
+    const handleUpload = () => {
+        if (file) {
+            const formData = new FormData();
+            formData.append("file", file);
 
-      fetch('http://localhost:3000/upload', {
-        method: 'POST',
-        body: formData,
-      })
-        .then((response) => response.json())
-        .then((data) => {
-          onFileUpload(data);
-        })
-        .catch((error) => {
-          console.error('Error uploading file:', error);
-        });
-    }
-  };
+            fetch("http://localhost:3001/upload", {
+                method: "POST",
+                body: formData,
+            })
+                .then((response) => response.json())
+                .then((data) => {
+                    setUploadedFile(data.fileName);
+                })
+                .catch((error) => {
+                    console.error("Error uploading file:", error);
+                });
+        }
+    };
 
-  return (
-    <div>
-      <input type="file" onChange={handleFileChange} />
-      <button onClick={handleUpload}>Upload</button>
-    </div>
-  );
+    return (
+        <div>
+            <input type="file" onChange={handleFileChange} />
+            <button onClick={handleUpload}>Upload</button>
+        </div>
+    );
 };
 
 export default FileUpload;
